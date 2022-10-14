1452444e2a690f19333c313efdaad38f092cf71c
ttuom1nen <ttuominen@outlook.com>
2022-10-14 16:11:50 +0300

    Set knex configs from knexfile based on env

---
+++ data/db.ts
@@ -0,0 +1,7 @@
+import knex from 'knex';
+import knexfile from '../knexfile';
+
+const env = process.env.NODE_ENV || 'development';
+const configOptions = knexfile[env];
+
+export default knex(configOptions);
