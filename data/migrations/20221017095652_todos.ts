147aa25747091624fd1f7c499c3b3b0e60215b62
ttuom1nen <ttuominen@outlook.com>
2022-10-17 13:01:35 +0300

    Update migration

--- data/migrations/20221017095652_todos.ts
+++ data/migrations/20221017095652_todos.ts
@@ -1,13 +1,12 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable("todo", tbl => {
+  return knex.schema.createTable("todos", tbl => {
     tbl.increments("id");
     tbl.string("task", 255).notNullable();
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTableIfExists("todo");
+  return knex.schema.dropTableIfExists("todos");
 }
-
--- data/seeds/todos.ts
+++ data/seeds/todos.ts
@@ -2,10 +2,10 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("todo").del();
+    await knex("todos").del();
 
     // Inserts seed entries
-    await knex("todo").insert([
+    await knex("todos").insert([
         { id: 1, task: "rowValue1" },
         { id: 2, task: "rowValue2" },
         { id: 3, task: "rowValue3" }
