889b9ad39bec2e6341fb9a7fd114be0c7c2ef2aa
ttuom1nen <ttuominen@outlook.com>
2022-10-17 14:00:51 +0300

    Rename todos router import

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
-import router from "./src/todos/todos.routes";
+import todosRouter from "./src/todos/todos.routes";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use(router);
+app.use(todosRouter);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
