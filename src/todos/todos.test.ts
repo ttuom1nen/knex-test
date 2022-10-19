09828ed6144e370573df3fc1deb91804e5fd34e1
ttuom1nen <ttuominen@outlook.com>
2022-10-19 16:50:34 +0300

    Add testing for post todos

--- src/todos/todos.test.ts
+++ src/todos/todos.test.ts
@@ -2,13 +2,26 @@ const request = require("supertest");
 const baseURL = "http://localhost:3008";
 
 describe("GET /todos", () => {
-  it("should return 200", async () => {
+  it("return 200", async () => {
     const response = await request(baseURL).get("/todos");
     expect(response.statusCode).toBe(200);
   });
 
-  it("should return todos", async () => {
+  it("return todos", async () => {
     const response = await request(baseURL).get("/todos");
     expect(response.body.length >= 1).toBe(true);
+  });
+});
+
+describe("POST /todos", () => {
+  const newTodo = { task: "New task created by test" };
+
+  it("returns the id of inserted todo", async () => {
+    const response = await request(baseURL)
+      .post("/todos")
+      .set("Content-type", "application/json")
+      .send(newTodo);
+
+    expect(response.body[0].hasOwnProperty("id")).toBe(true);
   });
 });
