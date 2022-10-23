9d7beb1ecc20641cbd5254e305ff79ada463f315
ttuom1nen <ttuominen@outlook.com>
2022-10-23 23:06:31 +0300

    Add route for patch todo

--- src/todos/todos.routes.ts
+++ src/todos/todos.routes.ts
@@ -29,13 +29,40 @@ todosRouter.get(
   }
 );
 
+todosRouter.patch(
+  "/todos",
+  validateBody<Todo>(todosSchema),
+  async (req: Request, res: Response): Promise<void> => {
+    const { id, task }: Todo = req.body;
+
+    const updatedTodo = await knex("todos").where({ id }).update(
+      {
+        task,
+        updated_at: knex.fn.now(),
+      },
+      ["id", "task", "updated_at"]
+    );
+
+    if (!updatedTodo.length) {
+      res.status(404);
+    }
+
+    res.send(updatedTodo);
+  }
+);
+
 todosRouter.post(
   "/todos",
   validateBody<Todo>(todosSchema),
   async (req: Request, res: Response): Promise<void> => {
     const { task } = req.body;
 
-    const id: Todo = await knex("todos").insert({ task }).returning("id");
+    const newTodo = {
+      task,
+      created_at: knex.fn.now(),
+    };
+
+    const id: Todo = await knex("todos").insert(newTodo).returning("id");
 
     res.status(200).send(id);
   }
