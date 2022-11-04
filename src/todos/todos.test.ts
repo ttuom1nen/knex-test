831112a682f42c7118c52fcaaac14822ff0bfdeb
ttuom1nen <ttuominen@outlook.com>
2022-11-04 21:52:22 +0200

    Add patch test

--- src/todos/todos.test.ts
+++ src/todos/todos.test.ts
@@ -1,27 +1,42 @@
 const request = require("supertest");
 const baseURL = "http://localhost:3008";
 
-describe("GET /todos", () => {
-  it("return 200", async () => {
-    const response = await request(baseURL).get("/todos");
-    expect(response.statusCode).toBe(200);
+describe("Todos endpoints", () => {
+  describe("GET /todos", () => {
+    it("returns 200", async () => {
+      const response = await request(baseURL).get("/todos");
+      expect(response.statusCode).toBe(200);
+    });
+
+    it("returns todos", async () => {
+      const response = await request(baseURL).get("/todos");
+      expect(response.body.length >= 1).toBe(true);
+    });
   });
 
-  it("return todos", async () => {
-    const response = await request(baseURL).get("/todos");
-    expect(response.body.length >= 1).toBe(true);
+  describe("POST /todos", () => {
+    const newTodo = { task: "New task created by test" };
+
+    it("returns the id of inserted todo", async () => {
+      const response = await request(baseURL)
+        .post("/todos")
+        .set("Content-type", "application/json")
+        .send(newTodo);
+
+      expect(response.body[0].hasOwnProperty("id")).toBe(true);
+    });
   });
-});
 
-describe("POST /todos", () => {
-  const newTodo = { task: "New task created by test" };
+  describe("PATCH /todos", () => {
+    const todo = { id: 1, task: "Modified task" };
 
-  it("returns the id of inserted todo", async () => {
-    const response = await request(baseURL)
-      .post("/todos")
-      .set("Content-type", "application/json")
-      .send(newTodo);
+    it("modifies existing todo", async () => {
+      const response = await request(baseURL)
+        .post("/todos")
+        .set("Content-type", "application/json")
+        .send(todo);
 
-    expect(response.body[0].hasOwnProperty("id")).toBe(true);
+      expect(response.statusCode).toBe(200);
+    });
   });
 });
